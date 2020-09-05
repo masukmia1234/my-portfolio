e47d812bd7562216032575e564e06aba3442b8fa
Masuk Mia <41548004+masukmia1234@users.noreply.github.com>
2020-09-05 20:10:56 +0600

    Add all my file

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dynamic Portfolio Template Using PHP/MYSQL
+i converted a static webpage template into a dynamic webpage and i also designed and creatd an "Admin Panel" for manage Website Content using Bootstrap and some common technologys like html,css and javascript.
+
+I used PHP & Mysql in backend.
+sql file is included with it you have to upload it on database first.
+
+you can check it out at
+https://iportfoliomonugiri.000webhostapp.com/
+
+admin panel
+https://myportfolio/admin
+admin id :masukmia.com
+pass: admin123
+
---
+++ include/iportfolio.sql
@@ -0,0 +1,364 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 29, 2020 at 12:45 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iportfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aboutus_setup`
+--
+
+CREATE TABLE `aboutus_setup` (
+  `id` int(11) NOT NULL,
+  `shortdesc` text NOT NULL,
+  `heading` text NOT NULL,
+  `subheading` text NOT NULL,
+  `longdesc` text NOT NULL,
+  `website` varchar(250) NOT NULL,
+  `dob` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `aboutus_setup`
+--
+
+INSERT INTO `aboutus_setup` (`id`, `shortdesc`, `heading`, `subheading`, `longdesc`, `website`, `dob`) VALUES
+(1, ' A developer is an individual that builds and create software and applications. He or she writes, debugs and executes the source code of a software application. A developer is also known as a software developer, computer programmer, programmer, software coder or software engineer', 'Full Stack Developer|Wordpress Developer', 'Hello, Iâ€™m a web designer Designer &amp; Front End Developer from 2018, Bangladesh. I hold a msity.', '\r\nTechopedia explains Developer\r\n\r\nA developer is the key individual behind all software applications. Generally, developers are well versed in at least one programming language and proficient in the art of structuring and developing software code for software or a program. Depending on job role and type of software developed, a developer may be classified as a software developer, application developer, mobile developer, Web developer, etc.\r\n\r\nAlthough the primary job role is writing code, a developer also may gather requirements for software, design or overall software architecture, software documentation and other related software development processes.\r\n', 'https://www.masukmia.com', '01 January, 1998');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_users`
+--
+
+CREATE TABLE `admin_users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `user_id` varchar(250) NOT NULL,
+  `user_pass` varchar(250) NOT NULL,
+  `user_access` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_users`
+--
+
+INSERT INTO `admin_users` (`id`, `username`, `user_id`, `user_pass`, `user_access`) VALUES
+(1, 'Masuk Mia', 'masukmia33021@gmail.com', 'admin123', 1),
+(2, 'Masuk Mia', 'masukmia33021@gmail.com', 'admin123', 1),
+(3, 'Masuk Mia', 'masukmia33021@gmail.com', 'admin123', 1),
+(4, 'Masuk Mia', 'masukmia33021@gmail.com', 'admin123', 1),
+(5, 'Masuk Mia', 'masukmia33021@gmail.com', 'admin123', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic_setup`
+--
+
+CREATE TABLE `basic_setup` (
+  `id` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `description` text NOT NULL,
+  `keyword` text NOT NULL,
+  `icon` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `basic_setup`
+--
+
+INSERT INTO `basic_setup` (`id`, `title`, `description`, `keyword`, `icon`) VALUES
+(1, 'Alex Smith', 'i am a web developer, i make website and php web portals .', 'web development,php development', 'icons8-computer-support-64.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `cname` varchar(250) NOT NULL,
+  `cemail` varchar(250) NOT NULL,
+  `csubject` text NOT NULL,
+  `cmessage` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `cname`, `cemail`, `csubject`, `cmessage`) VALUES
+(16, 'Shubham Seth', 'ristar@gmail.com', 'Partnership', 'i want to work with you , please contact me on my personal mobile no\r\nat <strong>7838403916</strong>'),
+(17, 'Sukriti Seth', 'sukriti46737@gmail.com', 'Complaint', 'i used your service but i dont like it you are worst servoces provider ever i am gonna sue you on fedral court and you have to pay a very big ammount to me becayuse of you i loss 20000 million dollars and you provide the worst service u have ever.'),
+(18, 'Masuk Mia', 'tworocksdeveloper@gmail.com', 'seo ', 'seo site resume is not right');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_setup`
+--
+
+CREATE TABLE `personal_setup` (
+  `id` int(11) NOT NULL,
+  `profilepic` varchar(250) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `twitter` varchar(200) NOT NULL,
+  `facebook` varchar(200) NOT NULL,
+  `instagram` varchar(200) NOT NULL,
+  `skype` varchar(200) NOT NULL,
+  `linkedin` varchar(200) NOT NULL,
+  `github` varchar(200) NOT NULL,
+  `homewallpaper` varchar(200) NOT NULL,
+  `professions` varchar(200) NOT NULL,
+  `location` text NOT NULL,
+  `mobile` varchar(200) NOT NULL,
+  `emailid` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `personal_setup`
+--
+
+INSERT INTO `personal_setup` (`id`, `profilepic`, `name`, `twitter`, `facebook`, `instagram`, `skype`, `linkedin`, `github`, `homewallpaper`, `professions`, `location`, `mobile`, `emailid`) VALUES
+(1, 'img.png', 'Masuk Mia', 'https://twitter.com/login', 'https://www.facebook.com/profile.php?id=100007597307697', 'https://www.instagram.com/oyeitsmg', '@oyeitsmg', 'https://www.linkedin.com/in/masuk-mia-5996861b2/', 'https://github.com/masukmia1234', 'profile_img.jpg', 'Web Developer,PHP Developer,Youtuber,Graphic Designer', 'Derai,sunamgonj,Sylhet ,BanglaDesh', '+008 01720565018', 'masukmia.cse33021@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portfolio`
+--
+
+CREATE TABLE `portfolio` (
+  `id` int(11) NOT NULL,
+  `projectname` varchar(250) NOT NULL,
+  `projectpic` varchar(250) NOT NULL,
+  `projectlink` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+INSERT INTO `portfolio` (`id`, `projectname`, `projectpic`, `projectlink`) VALUES
+(9, 'Javascript_01', 'pf2.jpg', '#https://github.com/masukmia1234/javascript-project'),
+(10, 'PHP Ecommerce site', 'pf3.jpg', '#https://github.com/masukmia1234/website/blob/master/Ecomerce%20Website_2%20Masud%20Media.zip'),
+(11, 'html and css ', 'pf6.jpg', 'https://github.com/masukmia1234/LCS'),
+(13, 'javascript', 'portfolio-1.jpg', 'https://github.com/masukmia1234/javascript-project'),
+(14, 'Html And Css', 'pf1.jpg', 'https://github.com/masukmia1234/html');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resume`
+--
+
+CREATE TABLE `resume` (
+  `id` int(11) NOT NULL,
+  `category` varchar(250) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `year` varchar(250) NOT NULL,
+  `ogname` varchar(250) NOT NULL,
+  `workdesc` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resume`
+--
+
+INSERT INTO `resume` (`id`, `category`, `title`, `year`, `ogname`, `workdesc`) VALUES
+(10, 'e', 'Bachelor Of Computer Science And Engineering', '2018-2020', 'Sylhet International University', 'I am a Web Developer.'),
+(11, 'pe', 'Mollapara Haji abdu Mia College', '2018-2019', 'LEcture at physcis', 'lecturer in physcis');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+CREATE TABLE `skills` (
+  `id` int(11) NOT NULL,
+  `skill` varchar(250) NOT NULL,
+  `score` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `skills`
+--
+
+INSERT INTO `skills` (`id`, `skill`, `score`) VALUES
+(12, 'HTML', '95'),
+(13, 'CSS', '90'),
+(14, 'PHP', '75'),
+(15, 'MYSQL', '75'),
+(16, 'Javascript', '60'),
+(17, 'Photoshop', '70'),
+(18, 'Bootstrap', '50'),
+(19, 'wordpress', '90');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinfo`
+--
+
+CREATE TABLE `userinfo` (
+  `id` int(11) NOT NULL,
+  `userq` varchar(250) NOT NULL,
+  `userv` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+INSERT INTO `userinfo` (`id`, `userq`, `userv`) VALUES
+(1, 'Birthday', '01 January ,1998'),
+(2, 'Age', '21'),
+(3, 'Website', 'www.masukmia.com'),
+(4, 'Degree', 'Bachelors of Science Computer Science And Engineering'),
+(5, 'Mobile', '01720565018'),
+(6, 'Email', 'tworocksdeveloper@gmail.com'),
+(7, 'City', 'Sylhet');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aboutus_setup`
+--
+ALTER TABLE `aboutus_setup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic_setup`
+--
+ALTER TABLE `basic_setup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personal_setup`
+--
+ALTER TABLE `personal_setup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `resume`
+--
+ALTER TABLE `resume`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aboutus_setup`
+--
+ALTER TABLE `aboutus_setup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `basic_setup`
+--
+ALTER TABLE `basic_setup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `personal_setup`
+--
+ALTER TABLE `personal_setup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `resume`
+--
+ALTER TABLE `resume`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `skills`
+--
+ALTER TABLE `skills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
